69174bd9131fc2b44959818d2c678ed3d33d392a
AIdan Maddock <aidanmaddock@AIdans-MacBook-Pro.local>
2026-06-08 15:21:48 +0100

    Convert emissions data to long and combine different emissions types

--- 02_r_scripts/00_emissions_cleaning.R
+++ 02_r_scripts/00_emissions_cleaning.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(tidyr)
 library(readxl)
 
 #=========================================================
@@ -7,14 +8,9 @@ library(readxl)
 # Description: This file attaches IPCC industry codes to
 # the EDGAR datasets (co2 and co2e) and aggregates by sector and country
 # Inputs: IEA_EDGAR_CO2_1970_2024.xlsx, EDGAR_AR5_GHG_1970_2024.xlsx
-# Outputs: co2_sector.csv, co2e_sector.csv
+# Outputs: emissions_sector.csv
 #=========================================================
 
-
-
-
-# Load sector-country CO2 data
-excel_sheets("00_raw_data/emissions/IEA_EDGAR_CO2_1970_2024.xlsx")
 
 co2 <- read_excel(
   "00_raw_data/emissions/IEA_EDGAR_CO2_1970_2024.xlsx", sheet = 2, skip = 9 #skip header
@@ -25,7 +21,7 @@ sort(unique(co2$ipcc_code_2006_for_standard_report))
 # Define map of IPCC industry codes (2006) to sectors
 sector_map <- tribble(
   ~ipcc_code_2006_for_standard_report, ~Sector,
-  "1.A.1.a", "Electricity & heat",
+  "1.A.1.a", "Energy",
   
   "1.A.2", "Industry",
   "2.A.1", "Industry",
@@ -60,7 +56,15 @@ co2_sector <- co2_sector_mapped %>%
     .groups = "drop"
   )
 
-# Load country_level CO2e data
+# Convert to long format for panel analysis
+co2_sector_long <- co2_sector %>%
+  pivot_longer(
+    cols = starts_with("Y_"),
+    names_to = "year",
+    values_to = "Emissions_co2"
+  )
+
+# Do the same for CO2e data
 co2e <- read_excel(
   "00_raw_data/emissions/EDGAR_AR5_GHG_1970_2024.xlsx", sheet = 2, skip = 9 #skip header
 )
@@ -81,9 +85,27 @@ co2e_sector <- co2e_sector_mapped %>%
     .groups = "drop"
   )
 
-# Remove placeholder dataframes
-rm(co2_sector_mapped, co2, co2e_sector_mapped, co2e)
+co2e_sector_long <- co2e_sector %>%
+  pivot_longer(
+    cols = starts_with("Y_"),
+    names_to = "year",
+    values_to = "Emissions_co2e"
+  )
+
+# Open country dataset for mapping to ISO
+country_groups <- read_csv("01_tidy_data/CountryGroupings.csv")
+
+# Combine emissions together
+emissions_combined <-co2_sector_long
+emissions_combined <- emissions_combined %>%
+  left_join(co2e_sector_long, by = c("Name", "Sector", "year")) %>%
+  left_join(country_groups, by = "Name") %>%
+  filter(!is.na(ISO)) %>% # Only keep countries in our sample
+  
+  mutate( 
+    year = substr(year, 3, nchar(year))
+  ) 
+  
 
 # Save outputs 
-write.csv(co2_sector, "01_tidy_data/co2_sector.csv")
-write.csv(co2e_sector, "01_tidy_data/co2e_sector.csv")
+write.csv(emissions_combined, "01_tidy_data/emissions_sector.csv")
